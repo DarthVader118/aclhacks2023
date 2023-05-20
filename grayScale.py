c63891a1b8595a11bc9104f27adbe031b70646cf
AnikethL <102996935+AnikethL@users.noreply.github.com>
2023-05-20 17:39:33 -0400

    line fix
    Doesn't have a seizure when hit with dashes

--- grayScale.py
+++ grayScale.py
@@ -6,7 +6,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-
+import time
 
 
 def grey(image):
@@ -39,11 +39,15 @@ def region(image):
 def display_lines(image, lines):
     lines_image = np.zeros_like(image)
     #make sure array isn't empty
+    
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line
             #draw lines on a black image
-            cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            try:
+                cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            except:
+                break
     return lines_image
 
 def average(image, lines):
@@ -52,11 +56,10 @@ def average(image, lines):
 
     if lines is not None:
       for line in lines:
-        print(line)
         x1, y1, x2, y2 = line.reshape(4)
         #fit line to points, return slope and y-int
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        print(parameters)
+
         slope = parameters[0]
         y_int = parameters[1]
         #lines on the right have positive slope, and lines on the left have neg slope
@@ -76,7 +79,7 @@ def average(image, lines):
     return np.array([left_line, right_line])
 
 def make_points(image, average):
-    print(average)
+    #print(average)
     
     try:
         slope, y_int = average
@@ -94,46 +97,53 @@ def make_points(image, average):
 
 
 '''##### DETECTING lane lines in image ######'''
-cam = cv2.VideoCapture("2023-aclhacks/hackathonThings/challenge.mp4")
-oldAvg=np.array([1,1])
+cam = cv2.VideoCapture("2023-aclhacks/hackathonThings/test2.mp4")
+oldAvg=np.array([])
 while True:
+    
     ret,image = cam.read()
-    copy = np.copy(image)
-    gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
-    copy =gauss(gray)
+    if ret:
+        copy = np.copy(image)
+        gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+        copy =gauss(gray)
 
-    
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    lower_yellow = np.array([75, 100, 100],dtype="uint8")
-    upper_yellow = np.array([255, 165, 165], dtype="uint8")
-    
-    mask_yellow = cv2.inRange(image_hsv, lower_yellow, upper_yellow)
-    mask_white = cv2.inRange(gray, 150, 175)
-    mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
-    mask_yw_image = cv2.bitwise_and(gray, mask_yw)
-    
-    edges = canny(mask_yw_image)
-    
-    isolated = region(edges)
-    
-    #DRAWING LINES: (order of params) --> region of interest, bin size (P, theta), min intersections needed, placeholder array, 
-    lines = cv2.HoughLinesP(isolated, 2, np.pi/180, 100, np.array([]), minLineLength=100, maxLineGap=100)
-    averaged_lines = average(copy, lines)
+        
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        lower_yellow = np.array([75, 100, 100],dtype="uint8")
+        upper_yellow = np.array([255, 165, 165], dtype="uint8")
+        
+        mask_yellow = cv2.inRange(image_hsv, lower_yellow, upper_yellow)
+        mask_white = cv2.inRange(gray, 150, 175)
+        mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+        mask_yw_image = cv2.bitwise_and(gray, mask_yw)
+        
+        edges = canny(mask_yw_image)
+        
+        isolated = region(edges)
+        
+        #DRAWING LINES: (order of params) --> region of interest, bin size (P, theta), min intersections needed, placeholder array, 
+        lines = cv2.HoughLinesP(isolated, 2, np.pi/180, 100, np.array([]), minLineLength=100, maxLineGap=100)
+        averaged_lines = average(copy, lines)
+        
+        if abs(averaged_lines[1][0]-averaged_lines[0][0])<=450:
+            averaged_lines=oldAvg
+            
+        oldAvg=averaged_lines
+        
+        
+        black_lines = display_lines(image, averaged_lines)
 
-    if math.isnan(averaged_lines[0][0])==True or math.isnan(averaged_lines[1][0])==True:
-        averaged_lines=oldAvg
-    oldAvg=averaged_lines
-    black_lines = display_lines(copy, averaged_lines)
-
-    #taking wighted sum of original image and lane lines image
-    lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
+        #taking wighted sum of original image and lane lines image
+        lanes = cv2.addWeighted(image, 0.8, black_lines, 1, 1)
     
     
-    cv2.imshow("iso", isolated)
-    cv2.imshow("final",lanes)
-    cv2.imshow("test", mask_yw_image)
-    k = cv2.waitKey(1)
-    if k != -1:
+        #cv2.imshow("iso", isolated)
+        cv2.imshow("final",lanes)
+        #cv2.imshow("test", mask_yw_image)
+        k = cv2.waitKey(1)
+        if k != -1:
+            break
+    else:
         break
     
 cam.release()
