95d6556129c5c2c40625e3511873e92d5519a502
AnikethL <102996935+AnikethL@users.noreply.github.com>
2023-05-20 12:13:07 -0400

    stuff

--- grayScale.py
+++ grayScale.py
@@ -1,12 +1,31 @@
 import cv2
-
+import numpy as np
 # this is code
 cam = cv2.VideoCapture("hackathonThings/test2.mp4")
 
 while True:
     ret, image = cam.read()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Imagetest',image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(7,7),0)
+    edge = cv2.Canny(blur,100,200)
+    
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    lower_yellow = np.array([75, 100, 100],dtype="uint8")
+    upper_yellow = np.array([255, 165, 165], dtype="uint8")
+    
+    mask_yellow = cv2.inRange(image_hsv, lower_yellow, upper_yellow)
+    mask_white = cv2.inRange(gray, 150, 1650)
+    mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+    mask_yw_image = cv2.bitwise_and(gray, mask_yw)
+    
+
+    
+    cv2.imshow("og", image)
+    cv2.imshow('gray',blur)
+    
+    cv2.imshow("mapper", mask_yw_image)
+    #cv2.imshow("test", lines)
+    
     k = cv2.waitKey(1)
     if k != -1:
         break
