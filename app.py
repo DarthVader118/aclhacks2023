5ec25a249bcf5e95a3a23d6a7eda6c2d83561fc4
AnikethL <102996935+AnikethL@users.noreply.github.com>
2023-05-21 10:38:43 -0400

    flask webpage stuff

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/execute_opencv_file', methods=['GET'])
+def execute_opencv_file():
+    try:
+        subprocess.run(['python', 'opencv_file.py'], check=True)
+        return 'OpenCV Python file executed successfully'
+    except subprocess.CalledProcessError as e:
+        return f'Error executing OpenCV Python file: {str(e)}'
+
+if __name__ == '__main__':
+    app.run()
